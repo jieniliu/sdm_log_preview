224599ba6bbd00de885b14887ab7677f476e9f2d
jieni <jieniliu@sding.me>
2025-05-15 19:57:57 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 
-module.exports = defineConfig({
+module.exports = {
+  publicPath: '/sdm_log_preview/', // 👈 用 publicPath 而不是 base
   transpileDependencies: true,
   devServer: {
     port: 8080,
     open: true
   }
-}) 
+}
